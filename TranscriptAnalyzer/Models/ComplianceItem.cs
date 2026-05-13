5e85a9b03b35ca9284267c59247da2a9017923ca
beyzaydnn <beyzaydnn@users.noreply.github.com>
2026-05-13 23:02:46 +0300

    remove TranscriptAnalyzer

--- TranscriptAnalyzer/Models/ComplianceItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TranscriptAnalyzer.Models;
-
-public class ComplianceItem
-{
-    public string Name { get; init; } = string.Empty;
-    public string[] Keywords { get; init; } = [];
-    public bool IsFound { get; set; }
-    public int Weight { get; init; }
-}
